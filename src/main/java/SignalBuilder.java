0efa3913048fb60e1bea40935b17acecfb8d830a
Patrick Indri <patrick.indri@outlook.it>
2020-10-13 20:47:26 +0200

    Tested exceptions in SignalBuilder

--- src/main/java/SignalBuilder.java
+++ src/main/java/SignalBuilder.java
@@ -6,38 +6,40 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SignalBuilder {
     // TODO: abstraction if more signal builders are necessary.
     // TODO: use boolean variables as well.
     // TODO: would it be useful to keep timestamps?
-    // TODO: info on temporal and variable bounds?
+    // TODO: maybe better exception handling?
 
     public static final String[] numNames = new String[]{"LIT101","AIT201","AIT202","AIT203"};
     public static final String[] boolNames = new String[]{};
 
-    public Signal<Record> build(String path, List<Integer> boolIndexes, List<Integer> doubleIndexes) throws IOException {
+    public Signal<Record> build(String path, List<Integer> boolIndexes, List<Integer> numIndexes) throws IOException {
         Signal<Record> signal = new Signal<>();
-        try {
-            BufferedReader buffReader = Files.newBufferedReader(Paths.get(path));
-            String[] header = buffReader.readLine().split(",");
+        BufferedReader buffReader = Files.newBufferedReader(Paths.get(path));
+        String[] header = buffReader.readLine().split(",");
 
-            String line;
-            double[] numValues;
-            boolean[] boolValues = new boolean[0]; // Dummy variable.
-            int time = 0;
+        String line;
+        double[] numValues;
+        boolean[] boolValues = new boolean[boolIndexes.size()]; // Dummy variable.
+        int time = 0;
 
-            while ((line = buffReader.readLine()) != null) {
-                numValues = Arrays.stream(line.split(",")).mapToDouble(Double::parseDouble).toArray();
-                signal.add(time, new Record(boolValues, numValues));
-                time++;
-            }
+        while ((line = buffReader.readLine()) != null) {
+//            numValues = Arrays.stream(line.split(",")).mapToDouble(Double::parseDouble).toArray();
+            List<String> input = Arrays.stream(line.split(",")).collect(Collectors.toList());
+            numValues = IntStream.range(0, header.length).filter(numIndexes::contains)
+                        .mapToDouble(i -> Double.parseDouble(input.get(i))).toArray();
+//            boolValues = IntStream.range(0, header.length).filter(boolIndexes::contains).mapToObj(input::get).map(Boolean::parseBoolean).toArray();
 
-            buffReader.close();
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            signal.add(time, new Record(boolValues, numValues));
+            time++;
         }
+
+        buffReader.close();
 
         return signal;
     }
--- src/test/java/SignalBuilderTest.java
+++ src/test/java/SignalBuilderTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -15,7 +16,7 @@ public class SignalBuilderTest {
     public void buildTest() throws IOException {
         SignalBuilder sb = new SignalBuilder();
         List<Integer> numIndexes = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
-        List<Integer> boolIndexes = new ArrayList<>(Arrays.asList());
+        List<Integer> boolIndexes = new ArrayList<>(Collections.emptyList());
         Signal<Record> signal = sb.build("data/test_data.csv", boolIndexes, numIndexes);
         assertFalse(signal.isEmpty());
 
@@ -23,6 +24,15 @@ public class SignalBuilderTest {
         double value = signal.valueAt(2).getNum(SignalBuilder.numNames[0]);
         double check = 124.4705; // Directly from file.
         assertEquals(value, check, 0);
-
     }
+
+    @Test(expected = IOException.class)
+    public void exceptionTest() throws IOException {
+        SignalBuilder sb = new SignalBuilder();
+        List<Integer> numIndexes = new ArrayList<>(Collections.emptyList());
+        List<Integer> boolIndexes = new ArrayList<>(Collections.emptyList());
+        Signal<Record> signal = sb.build("non_existent_file.csv", boolIndexes, numIndexes);
+        assertFalse(signal.isEmpty());
+    }
+
 }
