eb15c866bdcab69bceea32677947c83ea818cd85
Patrick Indri <patrick.indri@outlook.it>
2020-10-13 21:00:09 +0200

    Started STLMapper implementation

---
+++ src/main/java/STLMapper.java
@@ -0,0 +1,11 @@
+import elements.AbstractSTLNode;
+import it.units.malelab.jgea.representation.tree.Tree;
+
+import java.util.function.Function;
+
+public class STLMapper implements Function<Tree<String>, AbstractSTLNode> {
+    @Override
+    public AbstractSTLNode apply(Tree<String> stringTree) {
+        return null;
+    }
+}
